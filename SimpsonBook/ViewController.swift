6bd134240baa636db764976e62f8f12785851bf3
Ali Arif KURU <aliarifkuru@gmail.com>
2020-03-24 16:06:47 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Ali Arif Kuru on 24.03.20.
+//  Copyright © 2020 Ali Arif Kuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
